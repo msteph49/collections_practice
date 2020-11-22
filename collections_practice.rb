71923bac852dc2ee30cb7179b2cafaf0583551c3
Michael Stephens <mdstephens26@yahoo.com>
2020-11-22 12:19:33 -0500

    commit tree 46e7394caa1db5ff21e3cb327eed4d228217ada4

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,41 @@
+def sort_array_asc(array)
+    array.sort
+end
 
+def sort_array_desc(array)
+    array.sort {|x,y| y <=> x}
+end
+
+def sort_array_char_count(array)
+    array.sort_by {|x| x.length}
+end
+
+def swap_elements(array)
+    array[0], array[1], array [2] = array[0], array[2], array[1]
+end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(array)
+    array.find_all {|s| s[2] = "$"}
+end
+
+def find_a(array)
+    array.find_all {|s| s.start_with?("a")}
+end
+
+
+def sum_array(array)
+    sum = 0
+    array.inject(0) {|sum,x| sum + x}
+
+
+end
+
+def add_s(array)
+    new_array = array.each_with_index.collect {|element, index| element + "s" if !(index == 1)}
+    new_array[1] = array[1]
+    new_array
+end
